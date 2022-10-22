6125de329816304fa2e6865b813a0318dff38d85
ilissave <62884252+vazarch2@users.noreply.github.com>
2022-10-22 23:30:37 +0200

    Update CameraInfrarouge.java

--- src/Sensor/CameraInfrarouge.java
+++ src/Sensor/CameraInfrarouge.java
@@ -1,73 +1,139 @@
 package IA;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
-class CameraInfrarouge implements Sensor{
-	String[] sampleRed;
-	DatagramSocket dsocket;
-	public CameraInfrarouge(){
-	/*  	
-	try {
-      int port = 8888;
-      dsocket = new DatagramSocket(port);
+class CameraInfrarouge implements Sensor {
+	protected String sampleRed;
+	protected DatagramSocket dsocket;
+	protected String[] tabSampleRed;
+	protected byte[] buffer;
+	protected DatagramPacket packet;
 
-      byte[] buffer = new byte[2048];
+	public CameraInfrarouge() {
+		int port = 8888;
+		try {
 
-
-      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-      while (true) 
-      {
-        dsocket.receive(packet);
-
-        String msg = new String(buffer, 0, packet.getLength());
-
-        String[] palets = msg.split("\n");
-        
-        for (int i = 0; i < palets.length; i = i+2) 
-        {
-        	String[] coord = palets[i].split(";");
-        	int x = Integer.parseInt(coord[1]);
-        	int y = Integer.parseInt(coord[2]);
-            sampleRed[i] = x;
-            sampleRed[i+1] = y;
-        }
-        
-        
-        packet.setLength(buffer.length);
-      }
-     
-    } 
-    catch (Exception e) 
-    {
-      System.err.println(e);
-    }
-	*/	
-	int port = 8888; 
-      try {
-		dsocket = new DatagramSocket(port);
-	} catch (SocketException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+			dsocket = new DatagramSocket(port);
+			InetAddress serveur = InetAddress.getByName("192.168.1.255");
+			buffer = new byte[2048];
+			packet = new DatagramPacket(buffer, buffer.length, serveur, port);
+			dsocket.receive(packet);
+			sampleRed = new String(buffer, 0, packet.getLength());
+			tabSampleRed = sampleRed.split("\n");
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-      byte[] buffer = new byte[2048];
 
-      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-      sampleRed =  new String []{new String(buffer, 0, packet.getLength())};
+	public float getValue() {
+		return -1;
 	}
-		 public String getCoord(float[] sample) {
-		    return sampleRed[0];
-	}
-	@Override
 
-	public float getValue(float[] sample) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 	public void close() {
 		dsocket.close();
+
+	}
+
+	public void getData() throws IOException {
+		packet.setLength(buffer.length);
+		dsocket.receive(packet);
+		sampleRed = new String(buffer, 0, packet.getLength());
+		tabSampleRed = sampleRed.split("\n");
+	}
+
+	static int findMySelf(String[] tabSampleRed) {
+		int id = 0;
+		return id;
+	}
+
+	static int findNearMe(String[] tabSampleRed) {
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		//CameraInfrarouge cir = new CameraInfrarouge();
+		RegulatedMotor left = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor right = new EV3LargeRegulatedMotor(MotorPort.C);
+		left.forward();
+		right.forward();
 		
+		Touch tc = new Touch();
+		left.setAcceleration(800);
+		right.setAcceleration(800);
+
+		left.resetTachoCount();
+		right.resetTachoCount();
+	
+		while(tc.getValue()==0.0) {
+			try {
+				//int id = findMySelf(cir.tabSampleRed);
+				//int ne = findNearMe(cir.tabSampleRed);
+				//String[] coord = cir.tabSampleRed[id].split(";");
+				//String[] direct = cir.tabSampleRed[ne].split(";");
+				String r = new String("a,b,c,d,e");
+				String[] coord = r.split(",");
+				coord[1]="0";
+				coord[2]="0";
+				coord[3]="2";
+				coord[4]="2";
+				String[] direct= r.split(",");
+				direct[1]="0";
+				direct[2]="3";
+				
+				int selfx = Integer.parseInt(coord[1]);
+				int selfy = Integer.parseInt(coord[2]);
+				int directionx = Integer.parseInt(direct[1]);
+				int directiony = Integer.parseInt(direct[2]);
+
+				Delay.msDelay(100);
+				//cir.getData();
+				
+				//String[] coord2 = cir.tabSampleRed[id].split(";");
+				//vecteur de direction = position actuelle - position du palet
+				//vecteur de déplacement = position actuelle - ancienne position
+				directionx -= Integer.parseInt(coord[3]);
+				directiony -= Integer.parseInt(coord[4]);
+				selfx = Integer.parseInt(coord[3])-selfx;
+				selfy = Integer.parseInt(coord[4])-selfy;
+				double vectself = Math.sqrt(selfx * selfx + selfy * selfy);
+				double vectdirect = Math.sqrt(directionx * directionx + directiony * directiony);//à utiliser pour arcforward(angle,distance);
+				double scalaire = directionx * selfx + directiony * selfy;
+				double angle = Math.acos(scalaire / (vectself * vectdirect));
+				angle = Math.toDegrees(angle);
+				if (selfy > directiony) {
+					right.setSpeed((int) (right.getSpeed() - (angle * 10)));
+				} else if (directiony > selfy) {
+					left.setSpeed((int) (left.getSpeed() - (angle * 10)));
+				}
+				Delay.msDelay(100);
+				//cir.getData();
+			System.out.println(angle);
+				tc.touch.fetchSample(tc.samplePressed, 0);
+			} catch (Exception e) {
+				continue;
+			}
+
+	}
+
+}
+
+	public void run(boolean run) {
+		// TODO Auto-generated method stub
+
 	}
 }
