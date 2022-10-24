a2000a148335b6b558089a5431b55eaa137b5c0e
Gabriel Macé <macegabriel7238@gmail.com>
2022-10-24 16:50:33 +0200

    +filtre dans distance

--- src/Sensor/Distance.java
+++ src/Sensor/Distance.java
@@ -1,5 +1,7 @@
 package Sensor;
 
+import java.util.Arrays;
+
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.port.Port;
@@ -18,10 +20,29 @@ class Distance {
 		 sampleDistance = new float[distance.sampleSize()];
 	}
 	 
+public void trier() {
+		 double [] trie = Arrays.copyOf(sampleDistance, sampleDistance.length);
+		 Arrays.sort(trie);
+		 double q1 = trie[((int)(sampleDistance.length+3)/4-1)];
+		 double q3 = trie[((int)(3*sampleDistance.length+1)/4-1)];
+		 double borninf = trie[q1] - 1.5*(trie[q3]- trie[q1]);
+		 double bornsup = trie[q3] - 1.5*(trie[q3]- trie[q1]);
+		 int c = 0;
+		 for(int i = 0; i<sampleDistance.length; i++) {
+			 if(sampleDistance[i]<bornsup && sampleDistance[i]>borninf) {
+				 trie[c] = sampleDistance[i];
+				 c++;
+			 }
+		 }
+		 sampleDistance = Arrays.copyOf(trie, c);
+	 }
+	 
 	 public float getValue() {
 		 distance.fetchSample(sampleDistance, 0);
 		 return sampleDistance[0];
 	}
+	
+	 
 
 	
 	public void close() {
