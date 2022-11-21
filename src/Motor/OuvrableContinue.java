ea7457b05a0ef4d5f3da6d524b64ad457feffb2c
cedricVel <112876757+cedricVel@users.noreply.github.com>
2022-11-21 14:59:40 +0100

    maj OuvrableContinue

--- src/Motor/OuvrableContinue.java
+++ src/Motor/OuvrableContinue.java
@@ -2,21 +2,12 @@ package Motor;
 
 public interface OuvrableContinue {
 
-    public final static int ANGLE_OUVERTURE_TT = 90;
-    public final static int ANGLE_FERMETURE_TT = 0;
-    public final static int ANGLE_OUVERT_AVEC_PALET = 10; // lorsque les pinces ont saisie le palet
-    public final static int ANGLE_OUVERT_SANS_PALET = 45; // un angle intéressant pour relacher le palet
+    public final static int ANGLE_OUVERTUVERTURE_SANS_PALET = 360*4; // un angle intéressant pour relacher le palet
 
     /**
      * 
      * @param angle
      */
     public void ouvrir(int angle);
-
-    /**
-     * 
-     * @param angle
-     */
-    public void fermer(int angle);
 
 }
