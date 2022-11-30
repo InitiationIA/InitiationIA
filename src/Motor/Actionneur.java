e5038ae8a94026a699c453292a12779ea7e43b08
AsoyanArmen <108819049+AsoyanArmen@users.noreply.github.com>
2022-11-30 21:23:32 +0100

    Commentaires Javadoc Actionneur

--- src/Motor/Actionneur.java
+++ src/Motor/Actionneur.java
@@ -1,36 +1,29 @@
 package Motor;
-/**
- * La classe qui agrège les actioneur : 
- *  -les pinces
- *  -la motorisation (moteur + roue)
- *
- */
 
 import lejos.hardware.BrickFinder;
 import lejos.hardware.motor.*;
 import lejos.robotics.chassis.*;
-//import lejos.hardware.port.*;
 
 import lejos.robotics.navigation.MovePilot;
 public class Actionneur {
     
-    //Pince p = new Pince(MotorPort.B);
-    /**
-     * TODO: 
-     */
-    public final Wheel wheel1 ;
-    public final Wheel wheel2 ;
+    
+    protected final Wheel wheel1 ;
+    protected final Wheel wheel2 ;
     private final Chassis chassis ;
-   // private final Pince p;  
     private final MovePilot pilot ;
+    
 
+    /*
+     * Constructeur de l'actionneur
+     */
     public Actionneur() {
     	
-        wheel1 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(BrickFinder.getDefault().getPort("B")), 56).offset(-82);
-        wheel2 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(BrickFinder.getDefault().getPort("C")), 56).offset(82);
-       chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
-       // p = new Pince("A");
+        wheel1 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(BrickFinder.getDefault().getPort("B")), 56).offset(-80);
+        wheel2 = WheeledChassis.modelWheel(new EV3LargeRegulatedMotor(BrickFinder.getDefault().getPort("C")), 56).offset(80);
+        chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
         pilot = new MovePilot(chassis);
+        
     }
 
     public Actionneur(double vitesseLin,double accelerationLin) {
@@ -48,132 +41,164 @@ public class Actionneur {
         pilot.setAngularAcceleration(accAng);
     }
 
-	  
+    
+    /*
+     * retourne la valeur du tachomètre (en degré)
+     * de la roue gauche
+     */
+    public double getTachometer() {
+    	return wheel2.getMotor().getTachoCount();
+    }
+    
+	/*
+	 * retourne la valeur de l'accélération linéaire
+	 */
 	public double getAccelLigne() {
 	    return this.pilot.getLinearAcceleration();
 	}
 	
-	
+	/*
+	 * modifie l'accélération linéaire 
+	 */
 	public void setAccelLigne(double acceleration) {
 	    this.pilot.setLinearAcceleration(acceleration);
 	}
 
-	
+	/*
+	 * retourne l'accélération angulaire
+	 */
 	public double getAccelAngle() {
 	    return this.pilot.getAngularAcceleration();
 	}
 	
-	
+	/*
+	 * modifie l'accélaration linéaire
+	 */
 	public void setAccelAngle(double acceleration) {
 	    this.pilot.setAngularAcceleration(acceleration);
 	}
 
-	  
+	/*
+	 * retourne la vitesse linéaire
+	 */
 	public double getVitesseLigne() {
 	    return this.pilot.getLinearSpeed();
 	}  
 
-	
+	/*
+	 * modifie la vitesse linéaire
+	 */
 	public void setVitesseLigne(double speed) {
 	    this.pilot.setLinearSpeed(speed);
 	}
 
-	  
+	/*
+	 * retourne la vitesse linéaire maximale
+	 */
 	public double getMaxVitesseLigne() {
 	    return chassis.getMaxLinearSpeed();
 	}  
 
-	
+	/*
+	 * retourne la vitesse angulaire
+	 */
 	public double getVitesseAngle() {
 	    return this.pilot.getAngularSpeed();
 	}
 	  
-	
+	/*
+	 * modifie la vitesse angulaire
+	 */
 	public void setVitesseAngle(double speed) {
 	    this.pilot.setAngularSpeed(speed);
 	}
 	
-	 
+	/*
+	 * retourne la vitesse angulaire maximale
+	 */
 	public double getMaxVitesseAngle() {
 	    return chassis.getMaxAngularSpeed();
 	}  
 	  
 	
-	
+	/*
+	 * modifie l'angle pendant que le robot avance en arc
+	 */
 	public void arcAvant(double radius) {
 	    this.pilot.arcForward(radius);
-	  }
+	}
 
+	/*
+	 * modifie l'angle pendant que le robot recule en arc
+	 */
 	public void arcArriere(double radius) {
 		this.pilot.arcBackward(radius);
 	}
 
+	/*
+	 * avance en arc selon un gradient et un angle donné 
+	 */
 	public void arcSimple(double radius, double angle) {
 		this.pilot.arc(radius, angle);
 	}
-
+	
+	/*
+	 * traverse en arc selon un gradient et une distance
+	 */
 	public void traverseArc(double radius, double distance) {
 		this.pilot.travelArc(radius, distance, false);
 	}
 
+	/*
+	 * traverse en arc selon un gradient, une distance, en synchronisant
+	 */
 	public void traverseArc(double radius, double distance, boolean immediateReturn) {
 		this.pilot.arc(radius,  distance / (2 * Math.PI), immediateReturn);
 	}
 
 
 
-	  
+	/*
+	 * arrête le mouvement du robot
+	 */
     public void stop(){
         pilot.stop();
     }
 
+    /*
+     * retourne true si le robot est 
+     * en cours de déplacement
+     */
     public boolean estEnDeplacement(){
         return pilot.isMoving();
     }
     
+    /*
+     * avance le robot d'une distance donnée
+     */
   	public void avancer(double dis) {
   		this.pilot.travel(dis);
   	}
-  		
+  	
+  	/*
+  	 * avance le robot d'une distance donnée, en synchronisant
+  	 */
   	public void avancer(double dis, boolean b) {
   		this.pilot.travel(dis, b);
   	}
+  	
+  	/*
+  	 * recule le robot d'une distance donnée
+  	 */
   	public void reculer(double dis) {
-  		this.pilot.travel(dis);
+  		this.pilot.travel(dis, true);
   	}
   	
-	public void tourne(double ang) {
-  		this.pilot.rotate(ang);
+  	/*
+  	 * tourne le robot d'un angle donné
+  	 */
+  	public void tourne(double ang) {
+  		this.pilot.rotate(ang, true);
   	}
-  	public void tourne(double ang,boolean b) {
-  		this.pilot.rotate(ang, b);
-  	}
-  	
-	
-  	
-  
-  	
-  	
-		
-	
-	
-	// differnetial pilot = movePilot
-	
-	/*
-	 * TODO comment appeler des méthodes sur la variable pilot (voir MovePilotClass)
-	 */
-	
-	/*
-	 * utiliser Navigator: créer une instances des objets
-	 * 			Pose: position + direction
-	 * 			MoveController	
-	 */
-	
-	
-	public static void main(String[] args) {
-		
-
-		
-	}
 
 }
